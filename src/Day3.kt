7bff703a7315d70856101f14b254464a639e8e6a
Daschel Fortner <daschfortner@gmail.com>
2026-07-22 22:58:56 -0400

    day 3 brute forced

--- src/Day3.kt
+++ src/Day3.kt
@@ -1,6 +1,24 @@
 class Day3 : AdventDay("Day3") {
     override fun part1(): String {
-        return "!!! WARN not implemented"
+        var sum = 0
+        input.forEach { bank ->
+            var highest = 0
+
+            for (i in 0..<bank.length) {
+                for (j in i..<bank.length) {
+                    if (i != j) {
+                        val voltage = "${bank[i]}${bank[j]}".toInt()
+
+                        if (voltage > highest) {
+                            highest = voltage
+                        }
+                    }
+                }
+            }
+
+            sum += highest
+        }
+        return "$sum"
     }
 
     override fun part2(): String {
