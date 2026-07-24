3007b3c03d681483196efd67b5f24baa04684c32
Daschel Fortner <daschfortner@gmail.com>
2026-07-23 21:03:19 -0400

    add Day4 class

---
+++ src/Day4.kt
@@ -0,0 +1,9 @@
+class Day4 : AdventDay("Day4") {
+    override fun part1(): String {
+        return "!!! WARN not implemented"
+    }
+
+    override fun part2(): String {
+        return "!!! WARN not implemented"
+    }
+}
